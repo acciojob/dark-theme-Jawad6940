62c85b890e6f5c7e90b6efbeb18102cd018cdde9
Jawad TP <jawadtp248@gmail.com>
2022-07-20 15:45:41 +0530

    ok

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-//your code here
+//your code he
 
+const button_night= document.querySelector(".button_day");
+const night= document.querySelector(".day");
+// const btn= document.querySelector("#swap");
+
+function swapTheme(){
+    if(night.className=="day"){
+        night.className="night"
+        button_night.className="button_night"
+    }
+    else{
+        night.className="day"
+        button_night.className="button_day"
+    }
+
+}
+button_night.addEventListener("click", swapTheme);
